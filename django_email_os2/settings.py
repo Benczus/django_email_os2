a2f8221890f8f1e3aac63344c88891b777093269
bogdandy <bence.bogdandy@gmail.com>
2020-11-21 16:24:36 +0100

    fixed bug in static config

--- django_email_os2/settings.py
+++ django_email_os2/settings.py
@@ -121,7 +121,7 @@ PROJECT_ROOT= os.path.join(os.path.abspath(__file__))
 STATIC_ROOT=os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 CELERY_BROKER_URL= 'amqp://localhost'
